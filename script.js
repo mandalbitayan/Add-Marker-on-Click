fbabae09c009b34991db055c28cbec9b0e7f0aa3
Bitayan <mandalbitayan@gmail.com>
2026-07-09 12:09:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+require(["esri/Map","esri/views/MapView","esri/layers/GraphicsLayer","esri/Graphic"],
+    function(Map,MapView,GraphicsLayer,Graphic){
+
+        const map = new Map({
+            basemap:"streets"
+        });
+
+        const view = new MapView({
+            container:"viewDiv",
+            map:map,
+            center:[88.3639,22.5726],
+            zoom:12
+        });
+        
+        const graphiclayer = new GraphicsLayer()
+        map.add(graphiclayer)
+
+// add points on click 
+        view.on("click",function(e){
+            const point = {
+                type:"point",
+
+                longitude:e.mapPoint.longitude,
+                latitude:e.mapPoint.latitude
+            };
+
+            const symbol = {
+                type :"simple-marker",
+                color:"red",
+                size:10
+            };
+
+            const graphic = new Graphic({
+                geometry:point,
+                symbol:symbol
+            });
+            graphiclayer.add(graphic)
+
+        });
+
+    })
